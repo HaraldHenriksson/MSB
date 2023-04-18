8f5e283ba0b0b9819774231613536d26d6309df7
Harald <Harald@espinagar.se>
2023-04-18 15:05:22 +0200

    page one and two

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,23 @@ import './main.scss'
 
 // Import all of Bootstrap's JS
 import 'bootstrap/dist/js/bootstrap.js'
+
+const pageOne = document.getElementById("pageOne")
+const formBtn = document.getElementById("formBtn")
+const pageTwo = document.getElementById("pageTwo")
+const backBtn = document.getElementById("back")
+
+function togglePages() {
+    if (pageOne && pageTwo) {
+        pageOne.classList.toggle("hide");
+        pageTwo.classList.toggle("hide");
+    }
+}
+
+if (formBtn) {
+    formBtn.addEventListener("click", togglePages);
+}
+
+if (backBtn) {
+    backBtn.addEventListener("click", togglePages);
+}
